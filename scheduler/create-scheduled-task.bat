97fe8866b1066d43a97e8e75ebe5c01bc6cb11bf
Joao Castro <joo.luiz.98@outlook.com>
2024-05-27 23:33:42 -0300

    [fix] - adjusting command task to read all custom commands from profile

--- scheduler/create-scheduled-task.bat
+++ scheduler/create-scheduled-task.bat
@@ -33,17 +33,32 @@ if /i "%frequency%"=="daily" set "frequencySwitch=DAILY"
 if /i "%frequency%"=="weekly" set "frequencySwitch=WEEKLY"
 if /i "%frequency%"=="monthly" set "frequencySwitch=MONTHLY"
 
+:: Define the log file path in the same directory as the script
+set "logDir=%~dp0logs"
+if not exist "%logDir%" mkdir "%logDir%"
+set "logFile=%logDir%\%schedulerName%_log.txt"
+
+:: Mounts the command to run
 if "%commandType%"=="file" (
-    set "taskToRun=%command%"
+    set "taskToRun=%command% > %logFile% 2>&1"
 ) else (
-    set "taskToRun=cmd /c %command%"
+    set "taskToRun=powershell -NoProfile -ExecutionPolicy Bypass -Command . $PROFILE; %command% > %logFile% 2>&1"
 )
 
+echo Task to Run: %taskToRun%
+
+
 :: /IT in the end makes the job iterative with the user.
+:: /ru defines which user will be associated to execute this scheduler
+:: /RL defines the permissions the scheduler will have, if setted to highest the terminal running the commando must be running as admin
+:: /F Forces the creation of a new task, replacing any exsting task with the same name
+:: /IT This parameter makes the task interactive, which means the task will show up in he user screen
+:: All (or at lease some) of the parameters above must be editted to be in the job form and be passed to the schtask command if checked there.
+
 if "%frequencySwitch%"=="WEEKLY" (
-    schtasks /create /tn "%schedulerName%" /tr "%taskToRun%" /sc %frequencySwitch% /d %day% /st %time% /sd %startDate% /IT
+    schtasks /create /tn "%schedulerName%" /tr "%taskToRun%" /sc %frequencySwitch% /d %day% /st %time% /sd %startDate% /ru %USERNAME% /RL HIGHEST /F /IT
 ) else (
-    schtasks /create /tn "%schedulerName%" /tr "%taskToRun%" /sc %frequencySwitch% /st %time% /sd %startDate% /IT
+    schtasks /create /tn "%schedulerName%" /tr "%taskToRun%" /sc %frequencySwitch% /st %time% /sd %startDate% /ru %USERNAME% /RL HIGHEST /F /IT
 )
 
 endlocal
